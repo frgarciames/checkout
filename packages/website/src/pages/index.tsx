9d905ce77f4acbebdf1f847af07f745eed0df2db
frgarciames <fran17901@gmail.com>
2021-10-19 13:51:38 +0200

    Update title and description website

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -72,10 +72,7 @@ export default function Home(): JSX.Element {
     // }
   }, [])
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={siteConfig.title} description="Checkout SDK">
       <HomepageHeader />
       {/* <button
         onClick={() => {
